63dd982cb22892f55221618374d9d2fec0034c49
noodlemaster45 <nguyenquocdat12a7@gmail.com>
2023-05-05 17:49:42 +0700

    them xu ly thoi gian thanh toan

--- action/actionOrder.php
+++ action/actionOrder.php
@@ -3,15 +3,15 @@ session_start();
 include('../../faion/connection/Database.php');
 if(!empty($_GET['id'])){
     $id = $_GET['id'];
-    $date = date('y-m-d');
+    $date = date('Y-m-d H:i:s');
     $conn = new Database();
     $sql = "update `order`  od
-    set status=1, completed_at=".$date."
+    set status=1, completed_at='".$date."'
     where od.id =".$id;
     $conn->insert_update_delete($sql);  
     $_SESSION['message'] = "true";
 }else{
     $_SESSION['message'] = "false";
 }
-header('Location:/faion/index.php/admin/orders/');
+echo $date;
 ?>
